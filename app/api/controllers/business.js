a855c06438d59ee8def640b3c6b25410ea239f54
VladimirDanil <vladimir.danil25@mail.ru>
2021-06-28 08:16:15 -0700

    some update

--- app/api/controllers/business.js
+++ app/api/controllers/business.js
@@ -1,6 +1,6 @@
 
 const businessModel = require('../models/business');					    
-module.exports = {
+module.exports = {     
 	getById: function(req, res, next) {
 		console.log(req.body);
 		businessModel.findById(req.params.Id, function(err, result){
